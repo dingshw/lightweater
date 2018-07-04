0b22882956df7549de97daeb727a06efacffaab5
丁双文 <dingshuangwen@jd.com>
2018-07-04 18:03:48 +0800

    创建天气轻应用

---
+++ app.js
@@ -0,0 +1,19 @@
+App({
+  onLaunch () {
+    let that = this
+    wx.getSystemInfo({
+      success: function (res) {
+        that.globalData.systeminfo = res
+      },
+    })
+  },
+  globalData: {
+    // 是否保持常亮，离开小程序失效
+    keepscreenon:false,
+    systeminfo: {},
+    ak: '4tRDfmeg8kuxRk2E4ZgPkH0Fpli7ieIX',
+  },
+  setGeocoderUrl (address) {
+    return `https://api.map.baidu.com/geocoder/v2/?address=${address}&output=json&ak=${this.globalData.ak}`
+  },
+})
